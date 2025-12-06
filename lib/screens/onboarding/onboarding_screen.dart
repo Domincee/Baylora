0e127fcde80e8d3243bcbe2d183dbaa5af56ee48
Domincee <daseberos@gmail.com>
2025-12-06 23:57:12 +0800

    onboarding pages 3 initialayout and Pageviewr

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,26 +1,77 @@
 import 'package:baylora_prjct/config/routes.dart';
+import 'package:baylora_prjct/models/onboarding_model.dart';
 import 'package:flutter/material.dart';
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});  
 
-class OnboardingScreen extends StatelessWidget {
-  const OnboardingScreen({super.key});
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController();
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pageController.addListener(() {
+      setState(() {
+        currentIndex = _pageController.page?.round() ?? 0;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final data = OnboardingModel.getCategories();
+
     return Scaffold(
-      body: Center(
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           crossAxisAlignment: CrossAxisAlignment.center,
-           children: [
-            Text("Onboarding Screen"),
-            ElevatedButton(onPressed: () => Navigator.pushNamed(context, AppRoutes.login), child: Text("Next"))
-           
-           ],
-         ),
-        
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final item = data[index];
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(item.image),
+                    Text(item.title),
+                    Text(item.description),
+                  ],
+                );
+              },
+            ),
+          ),
+
+          // BOTTOM BUTTON
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (currentIndex == data.length - 1) {
+                  // LAST PAGE → GO TO MAIN
+                  Navigator.pushReplacementNamed(
+                      context, AppRoutes.main);
+                } else {
+                  // NEXT PAGE
+                  _pageController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeIn,
+                  );
+                }
+              },
+              child: Text(
+                currentIndex == data.length - 1 ? "Get Started" : "Next",
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-
-
